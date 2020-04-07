24a3bcc9b4cd05e5911171b39b93af97df72eb0c
Emmanuel Sedicol <sedicolemmanuel@gmail.com>
2020-04-07 19:43:27 +0100

    Finished Data import

---
+++ TEXAS_DATASET/my_lib.py
@@ -0,0 +1,18 @@
+# Collection of functions useful for analysing TeXas InPateint Dataset 
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+import seaborn as sns
+sns.set_style("darkgrid")
+
+from IPython.display import display, Markdown
+pd.set_option('display.max_columns', None)  
+
+import glob, os
+
+DEBUG = False
+SEED = 42
+
